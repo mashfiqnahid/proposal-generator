0e185857bb83c656be0a2fb09761011551da06b1
MD. Mashfiqur Rahman <mashfiqnahid@gmail.com>
2025-04-26 03:41:35 +0600

    Text Editor Added + Browser Title Changed

--- src/components/ModuleEditor/ModuleForm.tsx
+++ src/components/ModuleEditor/ModuleForm.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { Modal, Form, Input } from 'antd';
 import HourInput from './HourInput';
 import { ProjectModule } from '../../types/proposal.type';
+import { useEditor, EditorContent } from '@tiptap/react';
+import { StarterKit } from '@tiptap/starter-kit';
 
 interface ModuleFormProps {
     visible: boolean;
@@ -12,6 +14,18 @@ interface ModuleFormProps {
 
 const ModuleForm: React.FC<ModuleFormProps> = ({ visible, initialData, onSave, onCancel }) => {
     const [form] = Form.useForm(); // Create the form instance
+    const { description } = initialData
+
+    // Initialize Tiptap editor for the description field
+    const descriptionEditor = useEditor({
+        extensions: [StarterKit],
+        content: description,
+        onUpdate: ({ editor }) => {
+            form.setFieldsValue({
+                description: editor.getHTML(), // Update the form value with editor content
+            });
+        },
+    }, [description]);
 
     useEffect(() => {
         if (visible) {
@@ -24,19 +38,19 @@ const ModuleForm: React.FC<ModuleFormProps> = ({ visible, initialData, onSave, o
 
     const handleSave = () => {
         form.validateFields().then((values) => {
-            onSave(values);
+            onSave(values); // Pass form values (including description HTML) to onSave
             form.resetFields();
         });
     };
 
     return (
-        <Modal title={initialData?.name ? 'Edit ProjectModule' : 'Add New ProjectModule'} open={visible} onOk={handleSave} onCancel={onCancel}>
+        <Modal title={initialData?.name ? 'Edit Project Module' : 'Add New Project Module'} open={visible} onOk={handleSave} onCancel={onCancel}>
             <Form form={form} layout="vertical">
-                <Form.Item label="ProjectModule Name" name="name" rules={[{ required: true, message: 'Please enter module name' }]}>
+                <Form.Item label="Project Module Name" name="name" rules={[{ required: true, message: 'Please enter module name' }]}>
                     <Input />
                 </Form.Item>
                 <Form.Item label="Description" name="description" rules={[{ required: true, message: 'Please enter description' }]}>
-                    <Input.TextArea />
+                    <EditorContent editor={descriptionEditor} />
                 </Form.Item>
                 {/* Pass form instance to HourInput */}
                 <HourInput form={form} />
--- src/components/ProposalOverview.tsx
+++ src/components/ProposalOverview.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
 import { updateOverview } from '../features/proposalSlice';
+import { Editor, EditorContent, useEditor } from '@tiptap/react';
+import { StarterKit } from '@tiptap/starter-kit';
 
 const ProposalOverview: React.FC = () => {
   const dispatch = useAppDispatch();
   const overview = useAppSelector((state) => state.proposal.overview);
 
+  // Initialize the Tiptap editor
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: overview,
+    onUpdate: ({ editor }) => {
+      // Update the overview in the Redux store whenever the editor content changes
+      dispatch(updateOverview(editor.getHTML()));
+    },
+  },[overview]);
+
   return (
     <div>
       <h3>Overview</h3>
-      <textarea
-        value={overview}
-        onChange={(e) => dispatch(updateOverview(e.target.value))}
-      />
+      <EditorContent editor={editor} />
     </div>
   );
 };
